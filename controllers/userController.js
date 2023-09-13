2989241db84118f802156ff8a755f4667d5bb8e7
A. Urquiola <patchamama.go@gmail.com>
2023-09-13 16:14:20 +0200

    add option to update data of user (.put)

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,7 @@
 const bcrypt = require('bcrypt')
 const router = require('express').Router()
 const User = require('../models/user')
+const { userExtractor } = require('../utils/middleware')
 
 // Post a new user to the database
 router.post('/', async (request, response) => {
@@ -31,6 +32,44 @@ router.post('/', async (request, response) => {
   const savedUser = await user.save()
 
   response.status(201).json(savedUser)
+})
+
+// update a user data
+router.put('/:id', userExtractor, async (request, response) => {
+  // const userdata = await User.findById(request.params.id)
+  const { username, name, password, email } = request.body
+
+  const user = request.user
+
+  // only the user can update their own data
+  if (!user || request.params.id.toString() !== user.id.toString()) {
+    return response.status(401).json({ error: 'operation not permitted' })
+  }
+
+  // Check that the password is at least 3 characters long
+  if (!password || password.length < 3) {
+    return response.status(400).json({
+      error: '`password` is shorter than the minimum allowed length (3)',
+    })
+  }
+
+  // saltRounds is the number of times the password is hashed
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const updatedUser = await User.findByIdAndUpdate(
+    request.params.id,
+    {
+      username,
+      name,
+      email,
+      passwordHash,
+      // Add new fields to be updated here
+    },
+    { new: true }
+  )
+
+  response.json(updatedUser)
 })
 
 // Get all users
